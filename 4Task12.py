317b89a03cb501fde7f76b1fa6d4b76c573d14be
Jay Patel <35457113+Jaisaibaba236@users.noreply.github.com>
2018-01-16 16:50:47 -0400

    Update 4Task12.py

--- 4Task12.py
+++ 4Task12.py
@@ -1,12 +1,32 @@
-diction=dict()
 fin = open("Words.txt")
+diction=dict()
 
 def dictionary(string):
   for char in fin:
-    diction[char] += 1
+    diction[char] = 1
     if string in diction:
      return True
     else:
-     return False
-  
+     x = False
+  return x
+
 print(dictionary("Gyan"))
+
+List Lookup:
+  
+fin = ['abc','bcd','cde','def','efg']
+diction=dict()
+
+def dictionary(string):
+  j=0
+  for i in fin:
+    j=j+1
+    diction[j] = i
+    val = diction.values()
+    if string in val:
+     return True
+    else:
+     x = False
+  return x
+  print(diction)
+print(dictionary('2131'))
