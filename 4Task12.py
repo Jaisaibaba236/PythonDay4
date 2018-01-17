c599897722a519192d2accd543261c2f39ec349b
Jay Patel <35457113+Jaisaibaba236@users.noreply.github.com>
2018-01-17 15:22:45 -0400

    Update 4Task12.py

--- 4Task12.py
+++ 4Task12.py
@@ -30,3 +30,25 @@ def dictionary(string):
   return x
   print(diction)
 print(dictionary('2131'))
+
+
+
+
+fin = open("/home/inwkstudent/Desktop/words.txt")
+diction=dict()
+
+def dictionary(string):
+  j=0
+  for char in fin:
+    j=j+1
+    diction[j] = char
+  val = diction.values()
+  print(diction)
+  for char in diction:
+    if string in val:
+      x = True
+    else:
+      x = False   
+  return x
+  
+print(dictionary('aah'))
