7a5d980f3da149a3b19362119d9d4924673099b4
Jay Patel <35457113+Jaisaibaba236@users.noreply.github.com>
2018-01-17 15:02:23 +0000

    Update 4Task1.py

--- 4Task1.py
+++ 4Task1.py
@@ -1,22 +1,13 @@
-list1 = [1,2,3,4]
-list2 = [3,4,5,7,[2,3,12,23,34]]
-list1.append(list2)
-
-print(list1)
+list1 = [1,1,1,[1,1,1,[1,1,1,1,1],[1,1,1,1]]]
 
 def nested_sum(list1):
  sum1=0
- sum2=0
- sum3=0
  for i in list1:
   if type(i) == int:
     sum1 += i
   else:
-    for j in i:
-     if type(j) == int:
-       sum2 += j
-     else:
-       sum3 += sum(j)
- print(sum1+sum2+sum3)
+    sum1 += nested_sum(i)
+
+ return(sum1)
  
-nested_sum(list1)
+print(nested_sum(list1))
