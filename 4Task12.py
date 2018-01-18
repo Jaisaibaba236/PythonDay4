cc4cc4067aa95693cfb834ee4144086a9e0dfd9c
Jay Patel <35457113+Jaisaibaba236@users.noreply.github.com>
2018-01-18 14:28:48 -0400

    Update 4Task12.py

--- 4Task12.py
+++ 4Task12.py
@@ -1,54 +1,12 @@
-fin = open("Words.txt")
-diction=dict()
+import random
+fil = open("/home/jaisaibaba236/Desktop/words.txt")
+word_dictionary = dict()
 
-def dictionary(string):
-  for char in fin:
-    diction[char] = 1
-    if string in diction:
-     return True
-    else:
-     x = False
-  return x
+def search_word(fil,search_value):
+  for i in fil:
+    word_dictionary[i.strip()] = random.random()  
+  if search_value in word_dictionary:
+    return True
+  return False
 
-print(dictionary("Gyan"))
-
-List Lookup:
-  
-fin = ['abc','bcd','cde','def','efg']
-diction=dict()
-
-def dictionary(string):
-  j=0
-  for i in fin:
-    j=j+1
-    diction[j] = i
-    val = diction.values()
-    if string in val:
-     return True
-    else:
-     x = False
-  return x
-  print(diction)
-print(dictionary('2131'))
-
-
-
-
-fin = open("/home/inwkstudent/Desktop/words.txt")
-diction=dict()
-
-def dictionary(string):
-  j=0
-  for char in fin:
-    j=j+1
-    diction[j] = char
-  val = diction.values()
-  print(diction)
-  for char in diction:
-    if string in val:
-      x = True
-    else:
-      x = False   
-  return x
-  
-print(dictionary('aah'))
+print(search_word(fil,'receives'))
