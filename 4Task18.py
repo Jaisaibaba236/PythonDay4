ca282f8410534b963583985a14a16d1e35e966cd
Jay Patel <35457113+Jaisaibaba236@users.noreply.github.com>
2018-01-20 19:00:32 -0400

    Update 4Task18.py

--- 4Task18.py
+++ 4Task18.py
@@ -1,15 +1,14 @@
 fil = open("/home/jaisaibaba236/Desktop/words.txt")
-temp_list = []
 word_list = []
 
 def has_duplicate(fil):
   for i in fil:
-    word_list.append(i.strip())
+     word_list.append(i.strip())
   for value in word_list:
-    temp_list.append(value)
-    refined = word_list.pop(0)
-    if value in word_list:
-       return True
-    return False
-
+     x = word_list.pop(0)
+     if x in word_list:
+        print(x)
+        return True
+  return False
+     
 print(has_duplicate(fil))
